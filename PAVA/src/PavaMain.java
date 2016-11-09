9679596308eb4a4c33bf0f560e1577d4b93d698e
Phi <Phi@10.65.80.246>
2016-11-08 17:58:42 -0800

    Added name

--- PAVA/src/PavaMain.java
+++ PAVA/src/PavaMain.java
@@ -1,7 +1,7 @@
 
 public class PavaMain {
 	public static void main(String[] args) {
-		System.out.println("Adrian, Verena, Addison, Patrick");
+		System.out.println("Adrian, Verena, Addison, Patrick, Phillip");
 	}
 
 }
