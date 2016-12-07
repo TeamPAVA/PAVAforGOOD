3bc6f878ec7c7e50a7925c847f51bf2d5a71f852
patrickstevens <patrickstevens@10.0.0.132>
2016-12-07 13:00:21 -0800

    Added working menubar for donor

--- PAVA/src/GUI/MainFrame.java
+++ PAVA/src/GUI/MainFrame.java
@@ -2,28 +2,54 @@ package GUI;
 
 
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 // 
 public class MainFrame extends JFrame { //toolbar gets added to this class
 	
 	//Jframe instead of JPanel. This home frame will hold and show all of the other panels.
 	// Dimensions, names and such are just placeholders.
+	private JMenuBar menuBar;
+	private HomePanel homePanel;
+	private DonorPanel donorPanel;
+	//private OptionsPanel optionsPanel;
+	//private AboutPanel aboutPanel;
+	private JPanel current1;
 	public MainFrame(String userType) {
 		super();
 		int userTypeNum = Integer.parseInt(userType); //**********THIS IS WHAT WE PASS TO TOOLBAR
+		
 		setTitle("PAVAP");
 		setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        HomePanel myP = new HomePanel();
-        add(myP);
+        
+        //optionsPanel = new OptionsPanel();
+        //aboutPanel = new AboutPanel();
+        donorPanel = new DonorPanel();
+        homePanel = new HomePanel();
+        add(homePanel);
+        current1 = homePanel;
+
+        menuBar = new JMenuBar();
+        setupDonor();
+        
         
         if(userTypeNum == 0) {
-        	//admin
+        	//setupAdmin();
         } else if (userTypeNum == 1) {
-        	//donor
+        	//setupDonor();
         } else if (userTypeNum == 2) {
-        	//recipient
+        	//setupRec();
         }
+        this.setJMenuBar(menuBar);
+        
         
         
         
@@ -32,6 +58,279 @@ public class MainFrame extends JFrame { //toolbar gets added to this class
         
         pack();
 	}
+	/**
+	 * Helper method to set the current frame invisible 
+	 * @author Patrick Stevens
+	 */
+	private void setInvis() {
+		this.setVisible(false);
+	}
+	/**
+	 * Helper method to set the donor panel
+	 * @author Patrick Stevens
+	 */
+	private void addDonor() {
+		this.remove(current1);
+		this.add(donorPanel);
+		current1 = donorPanel;
+		this.revalidate();
+		this.repaint();
+	}
+	/**
+	 * Helper method to set the home panel
+	 * @author Patrick Stevens
+	 */
+	private void addHome() {
+		this.remove(current1);
+		this.add(homePanel);
+		current1 = homePanel;
+		this.revalidate();
+		this.repaint();
+	}
+	/**
+	 * Helper method to set the options panel
+	 * @author Patrick Stevens
+	 */
+	private void addOptions() {
+		this.remove(current1);
+		//this.add(optionsPanel);
+		//current1 = optionsPanel;
+		this.revalidate();
+		this.repaint();
+	}
+	/**
+	 * Helper method to set the about panel
+	 * @author Patrick Stevens
+	 */
+	private void addAbout() {
+		this.remove(current1);
+		//this.add(aboutPanel);
+		//current1 = aboutPanel;
+		this.revalidate();
+		this.repaint();
+	}
+	
+	/**
+	 * Method for setting up the donor menuBar
+	 * @author Patrick Stevens
+	 *
+	 */
+	private void setupDonor() {
+
+		        JButton about = new JButton("About");
+		        JButton options = new JButton("Options");
+		        JButton donate = new JButton("DONATE NOW");
+		        JButton home = new JButton("Home");
+		        JButton signOut = new JButton("Sign Out");
+
+		        signOut.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						LoginFrame login = new LoginFrame();
+						login.setVisible(true);
+						setInvis();
+					}
+		        	
+		        });
+		       options.addActionListener(new ActionListener() {
+		    	   @Override
+		    	   public void actionPerformed(ActionEvent e) {
+		        		//addOptions();
+		        	}
+		        	
+		        	
+		        });
+
+
+		       home.addActionListener(new ActionListener() {
+		    	   @Override
+		    	   public void actionPerformed(ActionEvent e) {
+		        		addHome();
+		    	   }
+		        	
+		        	
+		        });
+		        donate.addActionListener(new ActionListener() {
+		        	@Override
+		        	public void actionPerformed(ActionEvent e) {
+		        		addDonor();
+		        	}
+		        	
+		        	
+		        });
+
+
+		        
+		        about.addActionListener(new ActionListener() {
+		        	@Override
+		        	public void actionPerformed(ActionEvent e) {
+		        		//addAbout();
+		        	}
+		        	
+		        	
+		        });
+		        menuBar.add(home);
+		        menuBar.add(options);
+		        menuBar.add(Box.createHorizontalStrut(275));
+		        menuBar.add(donate);
+		        
+		        menuBar.add(Box.createHorizontalGlue());
+		        menuBar.add(about);
+		        menuBar.add(signOut);
+
+		    }
+	/**
+	 * Method for setting up the admin menuBar
+	 * @author Patrick Stevens
+	 *
+	 */
+	private void setupAdmin() {
+		
+		        JButton options = new JButton("Options");
+		        JButton about = new JButton("About");
+		        JButton home = new JButton("Home");
+		        JButton signOut = new JButton("Sign Out");
+
+
+		        menuBar.add(Box.createHorizontalGlue());
+
+
+		 
+		        
+		        signOut.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open loginFrame
+		        	}
+		        	
+		        	
+		        });
+		       options.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open optionsPanel
+		        	}
+		        	
+		        	
+		        });
+		       home.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open home
+		        	}
+		        	
+		        	
+		        });
+		   	        	
+		        
+		        about.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open about
+		        	}
+		        	
+		        	
+		        });
+		        menuBar.add(options);
+		       menuBar.add(home);
+		        menuBar.add(signOut);
+		        menuBar.add(about);
+
+
+
+
+		 
+
+
+		    }
+
+
+	/**
+	 * Method for setting up the recipient menuBar
+	 * @author Patrick Stevens
+	 *
+	 */
+	private void setupRec() {
+
+
+		       JButton options = new JButton("Options");
+	    	        JButton about = new JButton("About");
+		        JButton home = new JButton("Home");
+		        JButton signOut = new JButton("Sign Out");
+
+
+		        menuBar.add(Box.createHorizontalGlue());
+
+
+		 
+		        
+		        signOut.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open loginFrame
+		        	}
+		        	
+		        	
+		        });
+		       home.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open home
+		        	}
+		        	
+		        	
+		        });
+		   	        	
+		        
+		        about.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open about
+		        	}
+		        	
+		        	
+		        });
+		        options.addActionListener(new ActionListener() {
+
+
+			public void actionPerformed(ActionEvent e) {
+		        		//open optionsPanel
+		        	}
+		        	
+		        	
+		        });
+		        
+		        menuBar.add(home);
+		        menuBar.add(signOut);
+		        menuBar.add(about);
+		        menuBar.add(options);
+
+
+
+
+		 
+
+
+		    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
