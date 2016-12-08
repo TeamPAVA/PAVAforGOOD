bbd1a2ff1066174537f98699f6537b5dd7d602ab
Adrian Olives <adrianolives975@gmail.com>
2016-12-08 14:10:26 -0800

    JUnit/Micro Changes
    Deleted TestFrame as it is no longer needed, created a JUnit class
    called Tests because it is needed.

---
+++ PAVA/src/GUI/Tests.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Tests {
+
+        @Test
+        public void multiplicationOfZeroIntegersShouldReturnZero() {
+                HomePanel tester = new HomePanel(); // MyClass is tested
+
+                // assert statements
+                assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
+                assertEquals("0 x 10 must be 0", 0, tester.multiply(0, 10));
+                assertEquals("0 x 0 must be 0", 0, tester.multiply(0, 0));
+        }
+}
